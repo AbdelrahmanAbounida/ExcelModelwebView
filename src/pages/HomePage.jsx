7ff769949843c56debcef164a2ff0383fdd013c5
Abdelrahman Abounida <abdelrahmanaboneda@gmail.com>
2023-03-06 01:11:58 +0100

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -21,14 +21,14 @@ const Home = () => {
     console.log(type)
       switch(type){
           case 'Volume': console.log("volume");return RevenueState.Volume;
-          case 'Annual Volume Growth': return RevenueState.AnnualVolumeGrowth;
+          case 'Annual Volume Growth': return RevenueState.AnnualVolumeGrowth;+"%";
           case 'Price':  return RevenueState.Price;
-          case 'Annual Price Growth':  return RevenueState.AnnualPriceGrowth;
+          case 'Annual Price Growth':  return RevenueState.AnnualPriceGrowth;+"%";
 
           case 'Project capex item 1':  console.log("capex1"); return CapitalInvestmentState.capex1;
           case 'Project capex item 2':  return CapitalInvestmentState.capex2;
           case 'Total project capex':  return CapitalInvestmentState.total;
-          case 'Equity / (Debt + Equity)':  return CapitalInvestmentState.equity;
+          case 'Equity / (Debt + Equity)':  return CapitalInvestmentState.equity;+"%";
           case 'Debt repayment period (years)':  return CapitalInvestmentState.repayment;
 
           case 'Start of Capex':  return TimeLineState.StartCapex;
@@ -36,12 +36,12 @@ const Home = () => {
           case 'Start of operations':  return TimeLineState.StartOperations;
           case 'Asset life (years)':  return TimeLineState.AssetLife;
 
-          case 'Cost Item 1 (variable) as a share of revenue': console.log("capex1"); return OperationalCostState.CostItemVariable;
+          case 'Cost Item 1 (variable) as a share of revenue': console.log("capex1"); return OperationalCostState.CostItemVariable;+"%";
           case 'Cost Item 2 (fixed)':  return OperationalCostState.CostItemFixed;
-          case 'Cost Item 2 annual growth':  return OperationalCostState.CostItemAnnual;
+          case 'Cost Item 2 annual growth':  return OperationalCostState.CostItemAnnual;+"%";
 
-          case 'Income tax rate':  return OthersState.Income;
-          case 'Interest rate on debt':  return OthersState.Interest;
+          case 'Income tax rate':  return OthersState.Income;+"%";
+          case 'Interest rate on debt':  return OthersState.Interest;+"%";
           default: return 0;
 
       }
